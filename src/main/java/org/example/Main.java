e22f169fe44b850ee7e1a62e1f8804b6191c05dd
Yedil <yedilmyrzabek@gmail.com>
2024-09-19 15:22:31 +0500

    forth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,15 +12,6 @@ import static org.example.Circle.gg;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-
-
-
 
 
         
